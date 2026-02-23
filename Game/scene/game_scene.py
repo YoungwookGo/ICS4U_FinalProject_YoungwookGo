848371f8cd36d3f98b8b2c596c640e66375f40fc
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2026-02-23 04:32:57 -0500

    Score and Combo

--- Game/scene/game_scene.py
+++ Game/scene/game_scene.py
@@ -12,27 +12,34 @@ class GameScene(Scene):
     def __init__(self, game):
         super().__init__(game)
 
-        # Visuals ----------
-        self.font = pygame.font.Font("Game/asset/font/NotoSans-Medium.ttf", 48)
+        # Visuals -------------------------------
+        self.input_font = pygame.font.Font("Game/asset/font/NotoSans-Medium.ttf", 48)
+        self.ui_font = pygame.font.Font("Game/asset/font/NotoSans-SemiBold.ttf", 32)
 
         self.text_box = TextBox(
-            font=self.font,
+            font=self.input_font,
             size=(1000, 80),
             text_color=(255, 255, 255),
             idle_color=(70, 70, 80),
             active_color=(110, 110, 130),
         )
 
-        # Utilities ----------
+        # Utilities -----------------------------
         self.word_api = RandomWord()
 
-        # Entities ----------
+        # Entities ------------------------------
         self.enemies = pygame.sprite.Group()
         for i in range(3):
             word = self.word_api.get_word() or "offline"
             y = 200 + i * 120
             enemy = Enemy1(self.game, word, y)
             self.enemies.add(enemy)
+
+        # Game functions ------------------------
+        self.score = 0
+        self.combo = 0
+        self.max_combo = 0
+
         
 
     def manage_event(self, events):
@@ -53,6 +60,15 @@ class GameScene(Scene):
 
         for enemy in self.enemies:
             if enemy.word.lower() == text_input:
+                # Score manage
+                self.combo += 1
+                if self.combo > self.max_combo:
+                    self.max_combo = self.combo
+
+                gained = 10 + self.combo
+                self.score += gained
+
+                # Game function
                 y = enemy.rect.centery
                 enemy.kill()
 
@@ -62,6 +78,7 @@ class GameScene(Scene):
                 break
         else:
             # Runs when for loop don't breaks
+            self.combo = 0
             print("Typed wrong")
 
         self.text_box.text = ""
@@ -84,3 +101,12 @@ class GameScene(Scene):
 
         # Enemy
         self.enemies.draw(screen)
+
+        # UI
+        score_surf = self.ui_font.render(f"Score: {self.score}", True, (255, 255, 255))
+        combo_surf = self.ui_font.render(f"Combo: {self.combo}", True, (255, 255, 255))
+        comboM_surf = self.ui_font.render(f"Max Combo: {self.max_combo}", True, (255, 255, 255))
+
+        screen.blit(score_surf, (15, 10))
+        screen.blit(combo_surf, (15, 50))
+        screen.blit(comboM_surf, (15, 90))
