e523cd8b4f5a841f88beba1ab17e790ad6bc8f62
YoungwookGo <yw07kr@gmail.com>
2026-02-24 02:03:33 -0500

    add skill

--- Game/scene/game_scene.py
+++ Game/scene/game_scene.py
@@ -82,6 +82,8 @@ class GameScene(Scene):
 
         self.stage = 1
 
+        self.energy = 0
+
         # Statistic variables -------------------
         self.max_combo = 0
         self.kill_count = 0
@@ -92,7 +94,13 @@ class GameScene(Scene):
 
         # Manage every events from referenced classes
         for event in events:
-            # Manage events in text_box class
+            # Manage events in text_box class -----------------------
+            # Skill 1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_TAB:
+                self.skill1()
+                continue
+
+            # Text enter
             result = self.text_box.interact(event)
             if result == "enter":
                 print("Typed:", self.text_box.text)
@@ -114,11 +122,13 @@ class GameScene(Scene):
                 if self.combo > self.max_combo:
                     self.max_combo = self.combo
 
-                self.kill_count +=1
+                self.kill_count += 1
                 if self.kill_count >= self.stage * 20:
                     self.stage = self.kill_count // 20 + 1
                     bg_idx = (self.stage - 1) % len(self.bg_images)
                     self.background = self.bg_images[bg_idx]
+
+                self.energy += 10
 
                 # Reset enemy -----------------
                 new_word = self.word_api.get_word() or "ohno"
@@ -130,6 +140,26 @@ class GameScene(Scene):
             self.combo = 0
 
         self.text_box.text = ""
+
+    def skill1(self):
+        """Remove all enemies on screen and respawn them with new words."""
+        if self.energy < 100:
+            return  # not ready
+
+        self.energy= 0
+
+        for enemy in self.enemies:
+            new_word = self.word_api.get_word() or "ohno"
+            enemy.reset(new_word, enemy.rect.centery)
+
+            gained = 10 + self.combo + (self.stage - 1) 
+            self.score += gained
+            
+            self.kill_count += 1
+            if self.kill_count >= self.stage * 20:
+                    self.stage = self.kill_count // 20 + 1
+                    bg_idx = (self.stage - 1) % len(self.bg_images)
+                    self.background = self.bg_images[bg_idx]
 
 
     def update(self):
@@ -173,7 +203,14 @@ class GameScene(Scene):
         hp_surf = self.ui_font.render(f"HP: {self.hp}/{self.init_hp}", True, (255, 255, 255))
         score_surf = self.ui_font.render(f"Score: {self.score}", True, (255, 255, 255))
         combo_surf = self.ui_font.render(f"Combo: {self.combo}", True, (255, 255, 255))
+        energy_surf = self.ui_font.render(f"Energy: {self.energy}", True, (255, 255, 255))
+
+        kill_surf = self.ui_font.render(f"Kill: {self.kill_count}", True, (255, 255, 255))
+
 
         screen.blit(hp_surf, (20, 10))
         screen.blit(score_surf, (200, 10))
         screen.blit(combo_surf, (400, 10))
+        screen.blit(energy_surf, (600, 10))
+
+        screen.blit(kill_surf, (20, 45))
