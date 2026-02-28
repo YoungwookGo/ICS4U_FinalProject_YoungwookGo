7b12089f51a12714e1c64652b3e8e277d3823dff
YoungwookGo <yw07kr@gmail.com>
2026-02-28 18:54:30 -0500

    .

--- Game/entity/enemy1.py
+++ Game/entity/enemy1.py
@@ -5,5 +5,8 @@ class Enemy1(Enemy):
     Basic enemy type
     """
 
+    BASE_COLOR = (255, 255, 255)
+    BASE_SPEED = 120
+
     def __init__(self, game, y, speed_adj = 0, word = None):
         super().__init__(game, y, speed_adj=speed_adj, word=word)
